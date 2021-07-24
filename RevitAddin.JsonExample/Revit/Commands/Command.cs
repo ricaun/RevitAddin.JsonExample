018ba8aff08fdd8b69848ba47efe9ab514119ef5
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-07-24 11:55:35 -0300

    Adicionar arquivos de projeto.

---
+++ RevitAddin.JsonExample/Revit/Commands/Command.cs
@@ -0,0 +1,48 @@
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitAddin.JsonExample.Services;
+using System.Collections.Generic;
+
+namespace RevitAddin.JsonExample.Revit.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+
+            JavaScriptService javaScriptService = new JavaScriptService();
+
+            var obj = new MyClass() { Name = "Luiz", Last = "Cassettari", ElementId = new ElementId(100000) };
+
+            //obj.Skills.Add(new MySkills() { Name = "Revit", Level = 5 });
+            //obj.Skills.Add(new MySkills() { Name = "C#", Level = 8 });
+            //obj.Skills.Add(new MySkills() { Name = "Arduino", Level = 8 });
+
+            var str = javaScriptService.Serialize(obj);
+
+            System.Console.WriteLine(str);
+
+
+            System.Console.WriteLine(javaScriptService.Serialize(javaScriptService.Deserialize<MyClass>(str)));
+
+            return Result.Succeeded;
+        }
+
+        class MyClass
+        {
+            public string Name { get; set; }
+            public string Last { get; set; }
+            public ElementId ElementId { get; set; }
+            //public List<MySkills> Skills { get; set; } = new List<MySkills>();
+        }
+
+        class MySkills
+        {
+            public string Name { get; set; }
+            public int Level { get; set; }
+        }
+    }
+}
---
+++ RevitAddin.JsonExample/Services/JavaScriptService.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Web.Script.Serialization;
+
+namespace RevitAddin.JsonExample.Services
+{
+    public class JavaScriptService
+    {
+        private readonly JavaScriptSerializer javaScriptSerializer;
+        public JavaScriptService()
+        {
+            javaScriptSerializer = new JavaScriptSerializer();
+            RegisterConverter(new ElementIdConverter());
+        }
+
+        public void RegisterConverter(JavaScriptConverter javaScriptConverter)
+        {
+            javaScriptSerializer.RegisterConverters(new List<JavaScriptConverter>() { javaScriptConverter });
+        }
+
+        public string Serialize(object obj)
+        {
+            return javaScriptSerializer.Serialize(obj);
+        }
+
+        public T Deserialize<T>(string str)
+        {
+            return javaScriptSerializer.Deserialize<T>(str);
+        }
+
+        private class ElementIdConverter : JavaScriptConverter
+        {
+            private const string ElementIdKey = "ElementId";
+
+            public override IEnumerable<Type> SupportedTypes
+            {
+                get
+                {
+                    return new ReadOnlyCollection<Type>(new List<Type>(new Type[] { typeof(ElementId) }));
+                }
+            }
+
+            public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+            {
+                if (dictionary == null)
+                    throw new ArgumentNullException("Dictionary");
+                if (type == typeof(ElementId))
+                {
+                    return new ElementId((int)dictionary[ElementIdKey]);
+                }
+                return null;
+            }
+
+            public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+            {
+                ElementId elementId = obj as ElementId;
+                var result = new Dictionary<string, object>();
+                if (elementId != null)
+                {
+                    result[ElementIdKey] = elementId.IntegerValue;
+                }
+                return result;
+            }
+        }
+    }
+}
